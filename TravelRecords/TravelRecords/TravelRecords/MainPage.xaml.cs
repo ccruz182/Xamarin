5476f31b69f71013689d0d5e81dc780c8122d75f
César Cruz <cesar.cruz182@gmail.com>
2019-02-21 22:07:57 -0600

    MainPage with a basic login. Also a basic pagination

---
+++ TravelRecords/TravelRecords/TravelRecords/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecords
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+   
+    private void LoginButton_Clicked(object sender, EventArgs e)
+    {
+      bool emailValidation = string.IsNullOrEmpty(emailEntry.Text);
+      bool passValidation = string.IsNullOrEmpty(passwordEntry.Text);
+
+      if (emailValidation || passValidation)
+      {
+        errorMsgLabel.Text = "You must enter something in each one of the entries";
+        errorMsgLabel.TextColor = Color.Red;
+
+      }
+      else {
+        Navigation.PushAsync(new HomePage());
+      }
+    }
+  }
+}
