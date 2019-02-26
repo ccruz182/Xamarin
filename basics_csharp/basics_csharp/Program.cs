e56ed4ded77ca5ca2905017bc4d1574f6c69a3c3
César Cruz <cesar.cruz182@gmail.com>
2019-02-26 14:02:52 -0600

    Basic use of c sharp

---
+++ basics_csharp/basics_csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using basics_csharp.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics_csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SimpleMath p = new SimpleMath();
+      int x = p.Add(2, p.Add(3, p.Add(3, -3 + p.Add(3, 40))));
+      Console.WriteLine("Hello World" + x);
+      Console.ReadLine();
+    }    
+  }
+}
---
+++ basics_csharp/basics_csharp/classes/SimpleMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics_csharp.classes
+{
+  class SimpleMath
+  {
+    public int Add(int x, int y)
+    {
+      return x + y;
+    }
+  }
+}
